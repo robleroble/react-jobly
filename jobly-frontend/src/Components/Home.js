938962fc6741e9f91161a0fc65e62400f76b9bfe
Nick <nsand3@gmail.com>
2021-09-22 19:42:43 -0700

    designed navbar and home page for logged in or logged out users

--- jobly-frontend/src/Components/Home.js
+++ jobly-frontend/src/Components/Home.js
@@ -1,12 +1,42 @@
-import React from "react"
+import React from "react";
+import {
+  Container,
+  Button,
+  CardTitle,
+  CardSubtitle,
+  CardText,
+} from "reactstrap";
+import "../Stylings/Home.css";
 
 const Home = () => {
+  const loggedIn = true;
 
-    return (
-        <div>
-            <h2>Home</h2>
-        </div>
-    )
-}
+  const loggedInHome = (
+    <>
+      <CardText className="my-2">Welcome back, [USER]</CardText>
+    </>
+  );
+
+  const loggedOutHome = (
+    <>
+      <div className="my-2">
+        <Button className="mx-3">Login</Button>
+        <Button className="mx-3">Sign up</Button>
+      </div>
+    </>
+  );
+
+  return (
+    <div className="home-body d-flex align-items-center">
+      <Container className="d-flex flex-column align-items-center mb-5">
+        <CardTitle className="h2 fw-bolder my-2">Jobly</CardTitle>
+        <CardSubtitle className="h5 my-2">
+          All the jobs in one, convenient place.
+        </CardSubtitle>
+        {loggedIn ? loggedInHome : loggedOutHome}
+      </Container>
+    </div>
+  );
+};
 
 export default Home;
--- jobly-frontend/src/Components/NavBar.js
+++ jobly-frontend/src/Components/NavBar.js
@@ -1,41 +1,67 @@
-import React, {useState} from "react";
-import {NavLink} from "react-router-dom";
-import {Navbar, Nav, NavItem, Collapse, NavbarToggler} from "reactstrap";
-import "../Stylings/NavBar.css"
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+import { Navbar, Nav, NavItem, Collapse, NavbarToggler } from "reactstrap";
+import "../Stylings/NavBar.css";
 
 const NavBar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const loggedIn = false;
 
-    const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => setIsOpen(!isOpen);
 
-    const toggle = () => setIsOpen(!isOpen);
+  const loggedInNav = (
+    <>
+      <NavItem>
+        <NavLink className="nav-link" to="/companies">
+          Companies
+        </NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink className="nav-link" to="/jobs">
+          Jobs
+        </NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink className="nav-link" to="/profile">
+          Profile
+        </NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink className="nav-link" to="/logout">
+          Log out [username]
+        </NavLink>
+      </NavItem>
+    </>
+  );
 
+  const loggedOutNav = (
+    <>
+      <NavItem>
+        <NavLink className="nav-link" to="/login">
+          Login
+        </NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink className="nav-link" to="/signup">
+          Sign Up
+        </NavLink>
+      </NavItem>
+    </>
+  );
 
-    return (
-        <Navbar className="ml-5"color="dark" dark expand="md">
-            <NavLink className="ms-4 navbar-brand nav-link" to="/">Jobly</NavLink>
-
-            <NavbarToggler onClick={toggle} />
-                <Collapse isOpen={isOpen} navbar>
-                    <Nav className="ms-auto me-5" navbar>
-                        <NavItem>
-                            <NavLink className="nav-link" to="/companies">Companies</NavLink>
-                        </NavItem>
-                        <NavItem>
-                            <NavLink className="nav-link" to="/jobs">Jobs</NavLink>
-                        </NavItem>
-                        <NavItem>
-                            <NavLink className="nav-link" to="/profile">Profile</NavLink>
-                        </NavItem>
-                        <NavItem>
-                            <NavLink className="nav-link" to="/login">Login</NavLink>
-                        </NavItem>
-                        <NavItem>
-                            <NavLink className="nav-link" to="/signup">Sign Up</NavLink>
-                        </NavItem>
-                    </Nav>
-                </Collapse>
-        </Navbar>
-    )
-}
+  return (
+    <Navbar className="ml-5" color="dark" dark expand="md">
+      <NavLink className="ms-4 navbar-brand nav-link" to="/">
+        Jobly
+      </NavLink>
+      <NavbarToggler onClick={toggle} />
+      <Collapse isOpen={isOpen} navbar>
+        <Nav className="ms-auto me-5" navbar>
+          {loggedIn ? loggedInNav : loggedOutNav}
+        </Nav>
+      </Collapse>
+    </Navbar>
+  );
+};
 
 export default NavBar;
